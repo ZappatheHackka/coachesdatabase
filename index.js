e2587e62bef5532172551123c0757618d7cb95fe
Chris C <chriscottone1@gmail.com>
2025-01-29 20:01:22 -0500

    finished login page front-end w/basic functionality

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 const port = 3000;
+const password = "bamsketball";
+
 
 app.use(express.static(path.join(__dirname, "static")));
+app.use(express.urlencoded ({ extended: true}));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/static/index.html"));
     console.log(__filename);
+});
+
+app.get("/success", (req, res) => {
+    res.sendFile(path.join(__dirname, "/static/login.html"));
+})
+
+app.post('/login', (req, res) => {
+    if (req.body["username"] === password && req.body["password"] === password) {
+        res.redirect("/success");
+    }
+    else {
+        res.redirect('/');
+    }
 });
 
 app.listen(port, () => {
