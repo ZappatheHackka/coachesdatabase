21fdd40dafcb0438a8524ad3ed85de141aa39743
Chris <chriscottone1@gmail.com>
2025-02-02 08:57:16 -0500

    db init

---
+++ database.mjs
@@ -0,0 +1,17 @@
+import sequelize, { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+// Configuring .env for credentials
+dotenv.config();
+
+// Initiating JS connection to DB
+const connect = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+});
+
+// Making the sequelize instance available to the rest of the files 
+export default connect
