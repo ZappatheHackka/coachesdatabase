41e0de6ca70bb8ad11e02975518439b22a88cd22
Chris <chriscottone1@gmail.com>
2025-02-15 08:34:21 -0500

    login table

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-import { Coach, Client, Stats } from "./models.js";
+// import { Coach, Client, Stats } from "./src/models/models.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,18 +11,18 @@ const port = 3000;
 const password = "bamsketball";
 
 
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded ({ extended: true}));
 
 
 // Login page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/static/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
     console.log(__filename);
 });
 
 app.get("/success", (req, res) => {
-    res.sendFile(path.join(__dirname, "/static/login.html"));
+    res.sendFile(path.join(__dirname, "/public/home.html"));
 })
 
 app.post('/login', (req, res) => {
