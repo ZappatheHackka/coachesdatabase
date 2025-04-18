760da72ccf8b311dea436a50a3eb340a63adbb5f
ZappatheHackka <chriscottone1@gmail.com>
2025-04-17 22:37:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Middleware
 app.use(express.static(path.join(__dirname, "public")));
