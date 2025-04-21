5ef7d9b268ffb76b4596ed1761f7ae5b8c2c0f54
Chris <chriscottone1@gmail.com>
2025-04-21 09:26:53 -0400

    editbox

--- public/scripts/editForm.js
+++ public/scripts/editForm.js
@@ -3,4 +3,5 @@ var editButton = document.querySelector(".edit-box");
 editButton.addEventListener("click", () => {
     let form = document.querySelector("#popupform");
     form.style.display="block";
+    console.log("Hello");
 });
