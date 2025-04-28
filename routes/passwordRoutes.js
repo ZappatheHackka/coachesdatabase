3a32263521a457f0a364892312772020547a7daf
Chris <chriscottone1@gmail.com>
2025-04-28 10:40:24 -0400

    continuing password reset flow

--- routes/passwordRoutes.js
+++ routes/passwordRoutes.js
@@ -1,7 +1,9 @@
 import { Coach, Code } from '../src/models/models.js'
 import express from "express";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
+const saltRounds = 10;
 
 router.get('/pass', (req, res) => {
     res.render("pass_reset.ejs", { message: req.flash('error') });
@@ -17,12 +19,15 @@ router.post('/resetpass', async (req, res) => {
         });
         if (user) {
             const code = Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit numeric
+            let hashed_code = await bcrypt.hash(code, saltRounds);
             const user_Id = user.dataValues.id;
-            await Code.create({
-                code: code,
+            req.session.coach = user_Id;
+            let codeObject = await Code.create({
+                code: hashed_code,
                 coachID: user_Id,
                 used: false,
             });
+            req.session.codeId = codeObject.codeid; 
             console.log("thending email thir!!!!");
             req.session.userEmail = givenEmail;
             res.redirect(`/success`);
@@ -43,6 +48,14 @@ router.get('/success', (req, res) => {
     res.render("code_sent.ejs", {
         email: email
     });
+});
+
+
+router.post('/newpass', async (req, res) => {
+    let givenCode = req.body["newcode"];
+    let firstPass = req.body["pass1"];
+    let confirmPass = req.body["pass2"];
+    let coachID = req.session.coach;
 });
 
 export default router;
